215be795d6563c3c44684105054251abd08aba87
LucianoEsplai <lfrancoepifani@gmail.com>
2024-01-23 11:59:06 +0100

    Lazy React for image optimization

--- src/components/HomeGallery.jsx
+++ src/components/HomeGallery.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function HomeGallery({ photos }) {
   return (
-    <div className='grid grid-cols-4 grid-rows-2 gap-y-12'>
+    <div className='grid grid-cols-1 grid-rows-1 lg:grid-cols-4 lg:grid-rows-2 lg:gap-y-12'>
         {photos.map((photo, index) => {
             
             const animationClass = index < 4 ? 'animate-slideInFromLeft border-b-2 border-black' : 'animate-slideInFromRight border-t-2 border-black';
@@ -12,6 +12,7 @@ export default function HomeGallery({ photos }) {
                         src={photo}
                         alt={`Imagen ${index + 1}`}
                         className='w-full h-full object-cover'
+                        loading="lazy"
                     />
                     <div className="absolute top-0 left-0 w-full h-[225px] bg-black bg-opacity-35"></div>
                 </div>
--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Header from '../components/Header'
 
-function Collections() {
+export default function Collections() {
   return (
-    <div>Collections</div>
+    <div className='montserrat'>
+      <Header/>
+      <div>
+        <h2>Discover Timless Art</h2>
+        <p>By Art Gallery Elena</p>
+      </div>
+    </div>
   )
 }
 
-export default Collections
