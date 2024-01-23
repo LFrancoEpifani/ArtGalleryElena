e75858a2e28135df1615da31919a606e8009410f
LucianoEsplai <lfrancoepifani@gmail.com>
2024-01-23 13:46:09 +0100

    Updating Drop image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,7 @@ import Sevillana from '../assets/Sevillana.jpg'
 import Llanto from '../assets/Llanto.jpg'
 import HombreSentado from '../assets/HombreSentado.jpg'
 import ToreroMandala from '../assets/ToreroMandala.jpg'
-import { Link } from 'react-router-dom'
-import {Icon} from '@iconify/react'
+
 import Header from '../components/Header'
 
 export default function Home() {
@@ -23,11 +22,6 @@ export default function Home() {
     <main className='play absolute top-67 left-92 2xl:top-86 2xl:right-100 animate-slideInFromBottom font-normal text-[50px] text-black uppercase text-center tracking-wider border-h1'>
         <h1>Art Gallery Elena</h1>
     </main>
-    <div className='art-button flex justify-center items-center'>
-      
-      
-    </div>
-  
     </div>
   )
 }
