7db1174e0421a78d216c6a76fc052a47c08d4903
LucianoEsplai <lfrancoepifani@gmail.com>
2024-01-25 14:03:43 +0100

    Adding description ArtWork

--- src/components/ArtWork.jsx
+++ src/components/ArtWork.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function ArtWork({ name, price, size, image, id }) {
+export default function ArtWork({ name, price, size, image, id, description }) {
   return (
     <Link to={`/art/${id}`}>
       <div className="grid-item w-full md:w-1/2 lg:w-1/4 p-3 montserrat">
         <img
           src={image}
           alt={name}
-          className="object-contain w-full h-full "
+          className="object-contain w-full h-full rounded-sm"
         />
         <div className="text-center">
-          <h2 className="text-lg font-normal my-2">{name}</h2>
+          <h2 className="text-lg font-normal">{name}</h2>
+          <p className="text-xs text-gray-500">{description}</p>
           <p className="text-xs">{size}</p>
           <p className="text-md font-semibold">{price}</p>
         </div>
