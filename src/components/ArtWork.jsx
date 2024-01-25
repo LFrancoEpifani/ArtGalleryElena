b7e6cf74da1245d8e470fe42a086a3de86bea590
LucianoEsplai <lfrancoepifani@gmail.com>
2024-01-25 14:05:24 +0100

    Update

--- src/components/ArtWork.jsx
+++ src/components/ArtWork.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function ArtWork({ name, price, size, image, id, description }) {
+export default function ArtWork({ name, price, size, image, id }) {
   return (
     <Link to={`/art/${id}`}>
       <div className="grid-item w-full md:w-1/2 lg:w-1/4 p-3 montserrat">
@@ -11,8 +11,7 @@ export default function ArtWork({ name, price, size, image, id, description }) {
           className="object-contain w-full h-full rounded-sm"
         />
         <div className="text-center">
-          <h2 className="text-lg font-normal">{name}</h2>
-          <p className="text-xs text-gray-500">{description}</p>
+          <h2 className="text-lg font-normal my-2">{name}</h2>
           <p className="text-xs">{size}</p>
           <p className="text-md font-semibold">{price}</p>
         </div>
