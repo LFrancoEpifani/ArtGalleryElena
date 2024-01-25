e8da6ee46bfce37ffd339c7e98d08b05ee8f921e
LucianoEsplai <lfrancoepifani@gmail.com>
2024-01-25 16:39:04 +0100

    Responsive artDetails

--- src/pages/ArtDetails.jsx
+++ src/pages/ArtDetails.jsx
@@ -32,7 +32,7 @@ export default function ArtDetails() {
     setSelectedImage(selectedArt ? selectedArt.image : null);
 
     const paintRandom = randomPaint(artworks.filter((item) => item.id.toString() !== artId));
-    setRandomArts(paintRandom.slice(0, 5));
+    setRandomArts(paintRandom.slice(0, 12));
 
   }, [artId]);
 
@@ -55,15 +55,17 @@ export default function ArtDetails() {
           Go Back
         </p>
         </Link>
-      <div className='flex p-10 justify-evenly items-center'>
-      <div className='flex flex-col items-start gap-3'>
-        <img className='h-12 w-12 object-cover cursor-pointer' src={art.image} alt="" onClick={() => handleImageClick(art.image)}  />
-        <img className='h-12 w-12 object-cover cursor-pointer' src={art.house} alt="" onClick={() => handleImageClick(art.house)}  />
+      <div className='lg:flex p-10 justify-evenly items-center'>
+      <div className='flex flex-col gap-4 lg:gap-8 items-center'>
+        <div className='bg-black'>
+          <img className='object-contain w-90 shadow-xl border border-black' src={selectedImage} alt="" />
+        </div>
+        <div className='flex lg:flex-col items-start gap-3'>
+          <img className='h-12 w-12 object-cover cursor-pointer' src={art.image} alt="" onClick={() => handleImageClick(art.image)}  />
+          <img className='h-12 w-12 object-cover cursor-pointer' src={art.house} alt="" onClick={() => handleImageClick(art.house)}  />
+        </div>
       </div>
-      <div className='bg-black'>
-        <img className='object-contain w-90 shadow-xl border border-black' src={selectedImage} alt="" />
-      </div>
-       <div className=''>
+       <div className='my-4'>
         <h2 className='font-bold text-2xl my-2'>Elena</h2>
         <div className='my-2'>
           <p className='text-lg'>{art.name}</p>
@@ -76,22 +78,22 @@ export default function ArtDetails() {
         <hr className='border border-gray-300' />
        </div>
         <div className='flex flex-col gap-4 my-14'>
-          <button className='bg-black text-white py-2 px-14 rounded-full'>
+          <button className='bg-black text-white  py-4 lg:py-2 lg:px-14 rounded-full'>
               Purchase
           </button>
-          <button className='bg-white text-black border-black border py-2 px-14 rounded-full'>
+          <button className='bg-white text-black border-black border py-4 lg:py-2 lg:px-14 rounded-full'>
             Make an offer
           </button>
         </div>
        </div>
     </div>
-    <div className='px-[4vw] my-12'>
+    <div className='px-[12vw] my-12'>
         <h2 className='font-bold text-2xl my-8'>More ArtWorks</h2>
-        <div className='flex justify-center items-center gap-8'>
+        <div className='grid grid-cols-2 gap-2 items-start justify-start lg:grid-cols-4 lg:justify-center lg:items-center'>
           {randomArts.map((randomArt) => (
             <div key={randomArt.id}>
-              <img className='object-contain w-50 shadow-xl border border-black' src={randomArt.image} alt={randomArt.name} />
-              <div className="text-start my-2">
+              <img className='object-contain w-35 lg:w-50 shadow-xl' src={randomArt.image} alt={randomArt.name} />
+              <div className="text-start mb-4 lg:my-2">
                 <h2 className="text-lg font-semibold">{randomArt.name}</h2>
                 <p className="text-[12px] italic text-gray-500">{randomArt.description}</p>
                 <p className="text-xs text-gray-800 font-medium">{randomArt.size}</p>
